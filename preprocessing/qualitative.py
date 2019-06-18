755a75966b9086748918e4ff3373e0fb191f8f69
ririgi <lrpbaltazar@gmail.com>
2019-06-18 14:41:32 +0800

    add save to csv file per day

--- preprocessing/qualitative.py
+++ preprocessing/qualitative.py
@@ -13,37 +13,39 @@ def dayDetails(transact):
 	print("Total number of transactions: ", len(transact))
 	print("\n")
 
-def monthlyTransaction(directory):
+def monthlyTransaction(directory, outdirectory):
 	quali = pd.DataFrame()
 	for f in os.listdir(directory):
 		file = os.path.join(directory, f)
 		print("{}-{}-{}".format(file[-12:-8], file[-8:-6], file[-6:-4]))
 		transact = pd.read_csv(file)
 		dayDetails(transact)
-		quali = pd.concat([quali, getQualiFeatures(transact, quali)], axis = 0)
-	quali = quali.set_index('gigyaid')
-	print(quali)
+		transact = transact.loc[transact.gigyaid.notnull()]
+		quali = getQualiFeatures(transact)
+		outfile = os.path.join(outdirectory, "{}-{}-{}.csv".format(file[-12:-8], file[-8:-6], file[-6:-4]))
+		if len(quali) == 0:
+			continue
+		quali.to_csv(outfile)
 
 
-def getQualiFeatures(transact, quali):
-	transact = transact.loc[transact.gigyaid.notnull()]
+def getQualiFeatures(transact):
 	if len(transact) == 0:
 		return pd.DataFrame()
 	group = transact.groupby("gigyaid")
-	devicetype = group.apply(lambda x: x["devicetype"].unique()).reset_index(name="devicetype")
-	deviceos = group.apply(lambda x: x["deviceos"].unique()).reset_index(name="deviceos")
-	osversion = group.apply(lambda x: x["osversion"].unique()).reset_index(name="osversion")
-	ipaddress = group.apply(lambda x: x["ipaddress"].unique()).reset_index(name="ipadress")
-	browsertype = group.apply(lambda x: x["browsertype"].unique()).reset_index(name="browsertype")
-	connectivitytype = group.apply(lambda x: x["connectivitytype"].unique()).reset_index(name="connectivitytype")
-	screensize = group.apply(lambda x: x["screensize"].unique()).reset_index(name="screensize")
-	videoquality = group.apply(lambda x: x["videoquality"].unique()).reset_index(name="videoquality")
-	actiontaken = group.apply(lambda x: x["actiontaken"].unique()).reset_index(name="actiontaken")
+	devicetype = group.apply(lambda x: x["devicetype"].unique().tolist()).reset_index(name="devicetype")
+	deviceos = group.apply(lambda x: x["deviceos"].unique().tolist()).reset_index(name="deviceos")
+	osversion = group.apply(lambda x: x["osversion"].unique().tolist()).reset_index(name="osversion")
+	ipaddress = group.apply(lambda x: x["ipaddress"].unique().tolist()).reset_index(name="ipaddress")
+	browsertype = group.apply(lambda x: x["browsertype"].unique().tolist()).reset_index(name="browsertype")
+	connectivitytype = group.apply(lambda x: x["connectivitytype"].unique().tolist()).reset_index(name="connectivitytype")
+	screensize = group.apply(lambda x: x["screensize"].unique().tolist()).reset_index(name="screensize")
+	videoquality = group.apply(lambda x: x["videoquality"].unique().tolist()).reset_index(name="videoquality")
+	actiontaken = group.apply(lambda x: x["actiontaken"].unique().tolist()).reset_index(name="actiontaken")
 	
 	df = pd.concat([devicetype, deviceos, osversion, ipaddress, browsertype, connectivitytype, screensize, videoquality, actiontaken], axis = 1)
 	df = df.loc[:, ~df.columns.duplicated()]
-	# print(df)
+	df = df.set_index('gigyaid')
 	return(df)
 
 if __name__ == '__main__':
-	monthlyTransaction("../data/iWant/raw/10")
+	monthlyTransaction("../data/iWant/raw/10", "../data/iWant/processed/qualitative/10")
