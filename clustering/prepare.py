6629dc3664bc8fc3d3cd2fb5b99620fff955a3b8
ririgi <lrpbaltazar@gmail.com>
2019-06-25 04:00:19 +0800

    add conversion to ipaddress

--- clustering/prepare.py
+++ clustering/prepare.py
@@ -6,6 +6,8 @@ import os
 import pandas as pd
 import numpy as np
 
+from ip2geotools.databases.noncommercial import DbIpCity
+
 value = lambda x: x.strip("[]").replace("'", "").split(", ")
 converters={"actiontaken": value,
 			"devicetype": value,
@@ -49,3 +51,33 @@ def prepareDataDevice(data_dir, filename):
 
 	new_df.fillna(0, inplace=True)
 	return(new_df)
+
+def ipToCity(ipaddresses):
+	locations = []
+	for ip in ipaddresses:
+		try:
+			response = DbIpCity.get(ip, api_key='free')
+			loc = str(response.city) + "-" + str(response.region)
+			locations.append(loc)
+			print(ip, loc)
+		except:
+			pass
+	return locations
+
+def prepareLocation(data_dir, filename,  outfile):
+	cols = ["gigyaid", "ipaddress"]
+	df = pd.read_csv(os.path.join(data_dir, filename), dtype = str, low_memory = False, usecols = cols, converters = converters)
+	df["location_city"] = df["ipaddress"].apply(lambda x: ipToCity(x))
+	df.to_csv(os.path.join(outfile))
+	print("Done converting to location")
+	print("\n\n")
+
+if __name__ == '__main__':
+	# prepareLocation("../data/iWant/processed/preliminary", "september_2018.csv", "september_2018_location.csv")
+	# prepareLocation("../data/iWant/processed/preliminary", "october_2018.csv", "october_2018_location.csv")
+	prepareLocation("../data/iWant/processed/preliminary", "november_2018.csv", "november_2018_location.csv")
+	prepareLocation("../data/iWant/processed/preliminary", "2019-03-03.csv", "2019-03-03_location.csv")
+	prepareLocation("../data/iWant/processed/preliminary", "2019-01-06.csv", "2019-01-06_location.csv")
+	prepareLocation("../data/iWant/processed/preliminary", "2018-12-09.csv", "2018-12-09_location.csv")
+	prepareLocation("../data/iWant/processed/preliminary", "2019-02-03.csv", "2019-02-03_location.csv")
+
